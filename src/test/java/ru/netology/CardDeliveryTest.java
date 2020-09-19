9d124cd36f44ee8cefe4439f4c618562db1ae51f
Yuri Kopshev <yura.kopshev@gmail.com>
2020-09-19 15:03:46 +0200

    changed text files in accordance with the comments

--- src/test/java/ru/netology/CardDeliveryTest.java
+++ src/test/java/ru/netology/CardDeliveryTest.java
@@ -4,13 +4,18 @@ package ru.netology;
 import org.junit.jupiter.api.Test;
 import org.openqa.selenium.Keys;
 
-import static com.codeborne.selenide.Condition.exactText;
-import static com.codeborne.selenide.Condition.visible;
+import java.time.LocalDate;
+import java.time.format.DateTimeFormatter;
+
+import static com.codeborne.selenide.Condition.*;
 import static com.codeborne.selenide.Selectors.byText;
 import static com.codeborne.selenide.Selectors.withText;
 import static com.codeborne.selenide.Selenide.*;
 
 public class CardDeliveryTest {
+
+    String date = LocalDate.now().plusDays(3).
+            format(DateTimeFormatter.ofPattern("dd.MM.yyyy"));
 
 
 
@@ -19,12 +24,13 @@ public class CardDeliveryTest {
         open("http://localhost:9999");
         $("[type='text']").setValue("Москва").pressEnter();
         $("[data-test-id=date] input.input__control").doubleClick().sendKeys(Keys.BACK_SPACE);
-        $("[data-test-id=date] input.input__control").setValue("23.09.2020");
-        $$("[type='text']").last().setValue("Юрий Петров");
-        $$("[type='tel']").last().setValue("+79270000000");
+        $("[data-test-id=date] input.input__control").setValue(date);
+        $("[data-test-id= name] input.input__control ").setValue("Юрий Петров");
+        $("[data-test-id= phone] input.input__control ").setValue("+79270000000");
         $("[data-test-id='agreement']").click();
         $$("[type='button']").find(exactText("Забронировать")).click();
         $(withText("Успешно!")).waitUntil(visible,15000);
+        $("[data-test-id=notification] .notification__content").waitUntil(visible, 15000).shouldHave(text(date));
     }
 
     @Test
@@ -33,8 +39,8 @@ public class CardDeliveryTest {
         $("[type='text']").setValue("Москва").pressEnter();
         $("[data-test-id=date] input.input__control").doubleClick().sendKeys(Keys.BACK_SPACE);
         $("[data-test-id=date] input.input__control").setValue("19.09.2020");
-        $$("[type='text']").last().setValue("Юрий Петров");
-        $$("[type='tel']").last().setValue("+79270000000");
+        $("[data-test-id= name] input.input__control ").setValue("Юрий Петров");
+        $("[data-test-id= phone] input.input__control ").setValue("+79270000000");
         $("[data-test-id='agreement']").click();
         $$("[type='button']").find(exactText("Забронировать")).click();
         $(withText("Заказ на выбранную дату невозможен")).waitUntil(visible,3000);
@@ -44,10 +50,9 @@ public class CardDeliveryTest {
     void shouldCardDeliveryOrderFailByCity(){
         open("http://localhost:9999");
         $("[type='text']").setValue("Чертаново").pressEnter();
-        $("[data-test-id=date] input.input__control").doubleClick().sendKeys(Keys.BACK_SPACE);
-        $("[data-test-id=date] input.input__control").setValue("23.09.2020");
-        $$("[type='text']").last().setValue("Юрий Петров");
-        $$("[type='tel']").last().setValue("+79270000000");
+        $("[data-test-id=date] input.input__control").setValue(date);
+        $("[data-test-id= name] input.input__control ").setValue("Юрий Петров");
+        $("[data-test-id= phone] input.input__control ").setValue("+79270000000");
         $("[data-test-id='agreement']").click();
         $$("[type='button']").find(exactText("Забронировать")).click();
         $(withText("Доставка в выбранный город недоступна")).waitUntil(visible,3000);
@@ -57,10 +62,9 @@ public class CardDeliveryTest {
     void shouldCardDeliveryOrderFailByName(){
         open("http://localhost:9999");
         $("[type='text']").setValue("Майкоп").pressEnter();
-        $("[data-test-id=date] input.input__control").doubleClick().sendKeys(Keys.BACK_SPACE);
-        $("[data-test-id=date] input.input__control").setValue("23.09.2020");
-        $$("[type='text']").last().setValue("Adam Smith");
-        $$("[type='tel']").last().setValue("+79270000000");
+        $("[data-test-id=date] input.input__control").setValue(date);
+        $("[data-test-id= name] input.input__control ").setValue("Adam Smith");
+        $("[data-test-id= phone] input.input__control ").setValue("+79270000000");
         $("[data-test-id='agreement']").click();
         $$("[type='button']").find(exactText("Забронировать")).click();
         $(withText("Имя и Фамилия указаные неверно. Допустимы только русские буквы, пробелы и дефисы.")).waitUntil(visible,3000);
@@ -70,10 +74,9 @@ public class CardDeliveryTest {
     void shouldCardDeliveryOrderFailByTel(){
         open("http://localhost:9999");
         $("[type='text']").setValue("Москва").pressEnter();
-        $("[data-test-id=date] input.input__control").doubleClick().sendKeys(Keys.BACK_SPACE);
-        $("[data-test-id=date] input.input__control").setValue("23.09.2020");
-        $$("[type='text']").last().setValue("Юрий Петров");
-        $$("[type='tel']").last().setValue("+7927000000");
+        $("[data-test-id=date] input.input__control").setValue(date);
+        $("[data-test-id= name] input.input__control ").setValue("Юрий Петров");
+        $("[data-test-id= phone] input.input__control ").setValue("+7927000000");
         $("[data-test-id='agreement']").click();
         $$("[type='button']").find(exactText("Забронировать")).click();
         $(withText("Телефон указан неверно. Должно быть 11 цифр, например, +79012345678.")).waitUntil(visible,3000);
@@ -83,10 +86,9 @@ public class CardDeliveryTest {
     void shouldCardDeliveryOrderFailByAgreement(){
         open("http://localhost:9999");
         $("[type='text']").setValue("Москва").pressEnter();
-        $("[data-test-id=date] input.input__control").doubleClick().sendKeys(Keys.BACK_SPACE);
-        $("[data-test-id=date] input.input__control").setValue("23.09.2020");
-        $$("[type='text']").last().setValue("Юрий Петров");
-        $$("[type='tel']").last().setValue("+79270000000");
+        $("[data-test-id=date] input.input__control").setValue(date);
+        $("[data-test-id= name] input.input__control ").setValue("Юрий Петров");
+        $("[data-test-id= phone] input.input__control ").setValue("+79270000000");
         $$("[type='button']").find(exactText("Забронировать")).click();
         $("[data-test-id='agreement'].input_invalid ").shouldBe(visible);
     }
--- src/test/java/ru/netology/CardDeliveryTestLetter.java
+++ src/test/java/ru/netology/CardDeliveryTestLetter.java
@@ -2,41 +2,39 @@ package ru.netology;
 
 import org.junit.jupiter.api.Test;
 
-import static com.codeborne.selenide.Condition.exactText;
-import static com.codeborne.selenide.Condition.visible;
+import java.time.LocalDate;
+import java.time.format.DateTimeFormatter;
+
+import static com.codeborne.selenide.Condition.*;
 import static com.codeborne.selenide.Selectors.byText;
 import static com.codeborne.selenide.Selectors.withText;
 import static com.codeborne.selenide.Selenide.*;
 
 
 public class CardDeliveryTestLetter {
+
+    LocalDate today = LocalDate.now();
+    LocalDate meetingDate = LocalDate.now().plusDays(7);
+    String day = meetingDate.format(DateTimeFormatter.ofPattern("d"));
+
     @Test
     void shouldCardDeliveryOrderWeekLater() {
         open("http://localhost:9999");
         $("[type='text']").setValue("Ка");
         $(withText("Калининград")).click();
         $("[type=\"button\"].icon-button").click();
-        $$("td.calendar__day").find(exactText("27")).click();
-        $$("[type='text']").last().setValue("Юрий Петров");
-        $$("[type='tel']").last().setValue("+79270000000");
+        for (int i = 0; i < (meetingDate.getMonthValue() - today.getMonthValue()); i++) {
+            $("[data-step='1']").click();
+        }
+        $$("td.calendar__day").find(exactText(day)).click();
+        $("[data-test-id= name] input.input__control ").setValue("Юрий Петров");
+        $("[data-test-id= phone] input.input__control ").setValue("+79270000000");
         $("[data-test-id='agreement']").click();
         $$("[type='button']").find(exactText("Забронировать")).click();
         $(withText("Успешно!")).waitUntil(visible, 15000);
+        $("[data-test-id=notification] .notification__content").waitUntil(visible, 15000).shouldHave(text(meetingDate.format(DateTimeFormatter.ofPattern("dd.MM.yyyy"))));
     }
 
-    @Test
-    void shouldCardDeliveryOrderMonthLater() {
-        open("http://localhost:9999");
-        $("[type='text']").setValue("Ка");
-        $(withText("Калининград")).click();
-        $("[type=\"button\"].icon-button").click();
-        $("[class=\"calendar__arrow calendar__arrow_direction_right\"]").click();
-        $$("td.calendar__day").find(exactText("20")).click();
-        $$("[type='text']").last().setValue("Юрий Петров");
-        $$("[type='tel']").last().setValue("+79270000000");
-        $("[data-test-id='agreement']").click();
-        $$("[type='button']").find(exactText("Забронировать")).click();
-        $(withText("Успешно!")).waitUntil(visible, 15000);
-    }
+
 }
 
